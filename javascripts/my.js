7e995614f8d7f2251e6993081eb75839b101f97d
vVfishVv <vmpiro11@yahoo.com>
2014-04-08 19:26:19 -0400

    2nd Final

--- javascripts/my.js
+++ javascripts/my.js
@@ -4,17 +4,18 @@ function buttonMash()
 	var script;
 var typedYes = yes;
 var enterCount = 0;
+var bpm = 0;
 //input
 typedYes = prompt("Are you going to hit enter? (hit enter or type no)", "yes");
 //calculations
 while (typedYes == "yes") //start loop
 {
 	enterCount = enterCount + 1;
-
+    bpm = enterCount / 60;
 typedYes = alert("Hit enter to keep going or type anything else and enter to stop", "yes");
 }//end loop bracket
 //div get	
 script = document.getElementById('script')
 //output
-script_div.innerHTML = alert(""You mashed the enter key " + enterCount + " times!"")
+script_div.innerHTML = alert("You mashed the enter key at " + bpm + " Beats Per Minute!!")
 }
