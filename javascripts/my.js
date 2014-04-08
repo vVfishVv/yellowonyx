85605f5852053628b804cee0031daae00c79e293
vVfishVv <vmpiro11@yahoo.com>
2014-04-08 19:46:29 -0400

    added bs and jq

--- javascripts/my.js
+++ javascripts/my.js
@@ -12,10 +12,10 @@ while (typedYes == "yes") //start loop
 {
 	enterCount = enterCount + 1;
     bpm = enterCount / 60;
-typedYes = alert("Hit enter to keep going or type anything else and enter to stop", "yes");
+typedYes = prompt("Hit enter to keep going or type anything else and enter to stop", "yes");
 }//end loop bracket
 //div get	
-script = document.getElementById('script')
+script = document.getElementById('script');
 //output
-script_div.innerHTML = alert("You mashed the enter key at " + bpm + " Beats Per Minute!!")
+script_div.innerHTML = alert("You mashed the enter key at " + bpm + " Beats Per Minute!!");
 }
